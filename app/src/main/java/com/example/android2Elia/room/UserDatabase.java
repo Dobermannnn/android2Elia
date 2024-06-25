f334a5e6e901f1d0a22bd16064a928d942f32377
Dobermannnn <eliaabkasis13@gmail.com>
2024-06-25 22:14:56 +0300

    done

---
+++ app/src/main/java/com/example/android2Elia/room/UserDatabase.java
@@ -0,0 +1,9 @@
+package com.example.android2Elia.room;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {DbUser.class}, version = 1, exportSchema = false)
+public abstract class UserDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+}
